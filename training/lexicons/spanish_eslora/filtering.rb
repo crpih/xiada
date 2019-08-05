b5bc304a052119b217c9b54677f9e1f40039e7c4
Mario Barcala <barcala@nlpgo.com>
2019-08-05 17:50:04 +0200

    Some unneeded scripts removed from the repo.

--- training/lexicons/spanish_eslora/filtering.rb
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-while line = STDIN.gets
-  line.chomp!
-  puts line unless line =~ /VTP1P/ or line =~ /VTP3P/ or line =~ /VTP4S/ or line =~/DOAS/ or line =~/DONS/ or line=~/DOIS/
-end
-
