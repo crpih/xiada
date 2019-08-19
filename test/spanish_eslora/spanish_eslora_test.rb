751b7c7870a3c867145b50f15910894bc8f30b2d
Mario Barcala <barcala@nlpgo.com>
2019-08-19 13:43:31 +0200

    Some basic tests added from Spanish ESLORA

--- test/spanish_eslora/spanish_eslora_test.rb
+++ test/spanish_eslora/spanish_eslora_test.rb
@@ -1,13 +1,13 @@
 require "test_helper"
 
-class GalicianXiadaTaggingTests < Minitest::Test
+class SpanishEsloraTaggingTests < Minitest::Test
   def tagger(file_name)
-    command = " XIADA_PROFILE=galician_xiada ruby #{__dir__}/../../running/bin/xiada_tagger.rb -x #{__dir__}/../../running/galician_xiada/xml_values.txt -t -v -f #{__dir__}/#{file_name}.xml #{__dir__}/../../training/databases/galician_xiada/training_galician_xiada_escrita.db 2> #{__dir__}/#{file_name}.log | xmllint --format - > #{__dir__}/output/#{file_name}_new.xml "
+    command = " XIADA_PROFILE=spanish_eslora ruby #{__dir__}/../../running/bin/xiada_tagger.rb -x #{__dir__}/../../running/spanish_eslora/xml_values.txt -t -v -f #{__dir__}/#{file_name}.xml #{__dir__}/../../training/databases/spanish_eslora/training_spanish_eslora.db 2> #{__dir__}/#{file_name}.log | xmllint --format - > #{__dir__}/output/#{file_name}_new.xml "
     # puts "command:#{command}"
     `#{command}`
   end
 
-  STDERR.puts "galician #{__dir__}"
+  STDERR.puts "spanish #{__dir__}"
 
   Dir.glob("#{__dir__}/*.xml").each do |full_file_name|
     file_name = File.basename(full_file_name, ".*")
