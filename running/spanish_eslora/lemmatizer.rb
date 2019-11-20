9bdecf96b4f24d9059923af8a6fad3da83c4e4d4
Mario Barcala <barcala@nlpgo.com>
2019-11-20 11:00:04 +0100

    New lemmatization rules added to spanish eslora: ísimo/ísima/ísimos/ísimas and super

--- running/spanish_eslora/lemmatizer.rb
+++ running/spanish_eslora/lemmatizer.rb
@@ -2,9 +2,8 @@
 
 module LemmatizerSpanishEslora
   def lemmatize(word, tag, lemma)
+    # ito/ita/itos/itas suffix treatment
     if !lemma and tag !~ /^NP/ and word!=/áéíóú/
-      STDERR.puts "(lemmatize) word:#{word}, tag:#{tag}, lemma:#{lemma}"
-      # Diminutives
       if word =~ /ito$/
         new_lemma = word.delete_suffix("ito") << ("o")
       elsif word =~ /ita$/
@@ -17,6 +16,48 @@ module LemmatizerSpanishEslora
       if new_lemma
         infos = @dw.get_emissions_info(new_lemma, ["NCMS","AMS","VPMS","PQMS"])
         # STDERR.puts "new_lemma: #{new_lemma}, infos:#{infos}"
+        return new_lemma unless infos.empty?
+      end
+    end
+    # super prefix treatment
+    if !lemma
+      if word =~ /^super/
+        new_lemma = word.delete_prefix("super")
+        infos = @dw.get_emissions_info(new_lemma, nil)
+        return new_lemma unless infos.empty?
+      end
+    end
+    #ísimo/ísima/ísimos/ísimas treatment
+    rule_exceptions = ["tardísimo", "topísimo"]
+    if !lemma
+      if word =~ /ísimo$/
+        if rule_exceptions.include?(word)
+          new_lemma = word.delete_suffix("ísimo") << ("e")
+        else
+          new_lemma = word.delete_suffix("ísimo") << ("o")
+        end
+      elsif word =~ /ísima$/
+        if rule_exceptions.include?(word)
+          new_lemma = word.delete_suffix("ísima") << ("e")
+        else
+          new_lemma = word.delete_suffix("ísima") << ("o")
+        end
+      elsif word =~ /ísimos$/
+        if rule_exceptions.include?(word)
+          new_lemma = word.delete_suffix("ísimos") << ("e")
+        else
+          new_lemma = word.delete_suffix("ísimos") << ("o")
+        end
+      elsif word =~ /ísimas$/
+        if rule_exceptions.include?(word)
+          new_lemma = word.delete_suffix("ísimos") << ("e")
+        else
+          new_lemma = word.delete_suffix("ísimos") << ("o")
+        end
+      end
+      if new_lemma
+        infos = @dw.get_emissions_info(new_lemma, ["NCMS","AMS","W"])
+        STDERR.puts "new_lemma: #{new_lemma}, infos:#{infos}"
         return new_lemma unless infos.empty?
       end
     end
