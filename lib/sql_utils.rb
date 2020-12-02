18fbd61794f611c40f91c4f3998b3b2f6049c813
Mario Barcala <barcala@nlpgo.com>
2020-12-02 20:46:48 +0100

    Training exception bug solved

--- lib/sql_utils.rb
+++ lib/sql_utils.rb
@@ -4,6 +4,11 @@ class SQLUtils
 
   def self.escape_SQL(str)
     str = str.gsub(/'/,"''")
+    str
+  end
+
+  def self.escape_SQL_wildcards(str)
+    str = str.gsub(/'/,"''")
     str.gsub!("\*","%")
     str.gsub!("\?","_")
     str
