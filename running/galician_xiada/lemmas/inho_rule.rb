df298416ca75a687f7b7d5834a7104e62061bcd2
david <david@nlpgo.com>
2023-01-26 12:36:52 +0100

    Simplify rules.

--- running/galician_xiada/lemmas/inho_rule.rb
+++ running/galician_xiada/lemmas/inho_rule.rb
@@ -23,166 +23,26 @@ module Lemmas
 
       base, suffix, g, n = Regexp.last_match.captures
 
-      if base.end_with?('gu')
-        # amiguiño => amigo
-        # enruguiñas => enruga
-        # albondiguiñas => albóndiga
-        # estomaguiño => estómago
-        tilde_variants("#{base.delete_suffix('u')}#{g}#{n}").map { |v| query.copy(v, @tags) }
-      elsif base.end_with?('lc')
-        # solciño => sol (Scms)
-        # animalciños => animal (Scmp)
-        # descalciña => descalzo
-        search_base = base.delete_suffix('c')
-        common_sp_query = query.copy("#{search_base}z#{g}#{n}", @tags)
-        if n == 's' # When plural
-          [query.copy("#{search_base}es", @tags), common_sp_query]
-        else
-          [query.copy(search_base, @tags), common_sp_query]
-        end
-      elsif base.end_with?('qu')
-        # quiño/a/os/as
-        # plaquiñas => placas
-        # retaquiños => retacos
-        # musiquiña => música
-        # musiquiño => músico
-        # faisquiñas => faíscas
-        # periodiquiños => periódicos
-        # politiquiñas => políticas
-        # bosquiños => bosques
-        search_base = "#{base.delete_suffix('qu')}c#{g}#{n}"
-        [query.copy(search_base, @tags),
-         *tilde_variants(search_base).map { |v| query.copy(v, @tags) },
-         query.copy(search_base, @v_tags),
-         query.copy("#{base}e#{n}", @tags)]
-      elsif base.end_with?('onc')
-        if g == 'o'
-          # algodonciño => algodón
-          # cartonciños => cartóns
-          query.copy("#{base.delete_suffix('onc')}ón#{n}", @tags)
-        else
-          # cancionciña => canción
-          # chaponciñas => chaponas
-          search_base = base.delete_suffix('onc')
-          [query.copy("#{search_base}ón#{n}", @sfc_a0f_tags),
-           query.copy("#{search_base}ona#{n}", @sfc_a0f_tags)]
-        end
-      elsif base.end_with?('anc')
-        # garavanciño => garavanzo
-        # crianciñas => crianzas
-        # vranciño => vran
-        search_base = base.delete_suffix('anc')
-        [query.copy("#{search_base}anz#{g}#{n}", @tags),
-         query.copy("#{search_base}án#{n}", @tags),
-         query.copy("#{search_base}an#{n}", @tags)]
-      elsif base.end_with?('enc') && g == 'a'
-        # trenciñas => tranzas
-        # fervenciña => fervenza
-        query.copy("#{base.delete_suffix('c')}z#{g}#{n}", @tags)
-      elsif base.end_with?('inc')
-        # pinciñas => pinzas
-        # xardinciños => xardíns
-        search_base = base.delete_suffix('inc')
-        [query.copy("#{search_base}inz#{g}#{n}", @tags),
-         query.copy("#{search_base}ín#{n}", @tags)]
-      elsif base.end_with?('nd')
-        # segundiño => segundo
-        # brandiños => brandos
-        # pasandiño => pasando
-        # meirandiño => meirande
-        [query.copy("#{base}#{g}#{n}", @tags),
-         query.copy("#{base}e#{n}", @saiv_tags)]
-      elsif base.end_with?('rc')
-        # cogorciña => cogorza
-        # verciñas => verzas
-        # almorciño => almorzo
-        search_base = base.delete_suffix('c') 
-        common_sp_query = query.copy("#{search_base}z#{g}#{n}", @sa_tags)
-        if n == 's' # When plural
-          # calamarciños => calamares
-          [common_sp_query, query.copy("#{search_base}es", @sa_tags)]
-        else
-          # calorciño => calor
-          # altarciño => altar
-          # amorciño => amor
-          [common_sp_query, query.copy(search_base, @sa_tags)]
-        end
-      elsif %w[f m v x].include?(suffix)
-        # xefiño => xefe
-        # lumiño => lume
-        # suaviñas => suaves
-        # traxiños => traxes
-        [query.copy("#{base}#{g}#{n}", @sai_tags),
-         query.copy("#{base}e", @sai_tags)]
-      elsif base.end_with?('t')
-        # gatiños => gatos
-        # azoutiña => azouta
-        # tomatiños => tomates
-        # amantiñas => amantes
-        [query.copy("#{base}#{g}#{n}", @saiw_tags),
-         query.copy("#{base}e#{n}", @sav_tags)]
-      elsif %w[b p ñ].include?(suffix)
-        common_suffix_query = query.copy("#{base}#{g}#{n}", @sav_tags)
-        if suffix == 'b'
-          # barbiña => barba
-          # xoubiñas => xouba
-          # FIXME: Intended rule was: nubiñas => nubes
-          # nubiñas => nube
-          [common_suffix_query, query.copy("#{base}e", @sav_tags)]
-        else
-          # trapiño => trapo
-          # castañiñas => castañas
-          # viñiño => viño
-          common_suffix_query
-        end
-      elsif base.end_with?('ch')
-        # estuchiño => estuche
-        # churriñas => churras
-        [query.copy("#{base}e", @sav_tags),
-         query.copy("#{base}#{g}#{n}", @sav_tags)]
-      elsif base.end_with?('ll')
-        # ovelliñas => ovellas
-        query.copy("#{base}#{g}#{n}", @sav_tags)
-      elsif base.end_with?('rr')
-        # aforriños => aforros
-        query.copy("#{base}#{g}#{n}", @sav_tags)
-      elsif base.end_with?('d')
-        # cadradiños => cadrados
-        # todiñas => todas
-        # bigodiño => bigode
-        # humildiño => humilde
-        [query.copy("#{base}#{g}#{n}", @saiv_tags),
-         query.copy("#{base}e#{n}", @saiv_tags)]
-      elsif base.end_with?('ues')
-        search_base = base.delete_suffix('es')
-        if n == ''
-          if g == 'o'
-            # marquesiño => marqués
-            query.copy("#{search_base}és", @sav_tags)
-          elsif g == 'a'
-            # marquesiña => marquesa
-            query.copy("#{search_base}esa", @sav_tags)
-          end
-        else # When plural
-          if g == 'o'
-            # marquesiños => marqueses
-            query.copy("#{search_base}eses", @sav_tags)
-          else
-            # marquesiñas => marquesas
-            query.copy("#{search_base}esas", @sav_tags)
-          end
-        end
-      elsif base.end_with?('s')
-        # tesiños => tesos
-        # raposiña => raposa
-        # camisiñas => camisas
-        # tesiña => tese
-        # tosiña => tose
-        [query.copy("#{base}#{g}#{n}", @sav_tags),
-         query.copy("#{base}e#{n}", @sav_tags)]
-      else
-        # horiña, peliños, groliño, etc.
-        query.copy("#{base}#{g}#{n}", @tags)
+      case
+      when base.end_with?('gu') then apply_gu(query, base, g, n)
+      when base.end_with?('lc') then apply_lc(query, base, g, n)
+      when base.end_with?('qu') then apply_qu(query, base, g, n)
+      when base.end_with?('onc') then apply_onc(query, base, g, n)
+      when base.end_with?('anc') then apply_anc(query, base, g, n)
+      when base.end_with?('enc') && g == 'a' then apply_enc_a(query, base, g, n)
+      when base.end_with?('inc') then apply_inc(query, base, g, n)
+      when base.end_with?('nd') then apply_nd(query, base, g, n)
+      when base.end_with?('rc') then apply_rc(query, base, g, n)
+      when %w[f m v x].include?(suffix) then apply_fmvx(query, base, g, n)
+      when base.end_with?('t') then apply_t(query, base, g, n)
+      when %w[b p ñ].include?(suffix) then apply_bpnh(query, base, g, n)
+      when base.end_with?('ch') then apply_ch(query, base, g, n)
+      when base.end_with?('ll') then apply_ll(query, base, g, n)
+      when base.end_with?('rr') then apply_rr(query, base, g, n)
+      when base.end_with?('d') then apply_d(query, base, g, n)
+      when base.end_with?('ues') then apply_ues(query, base, g, n)
+      when base.end_with?('s') then apply_s(query, base, g, n)
+      else apply_default(query, base, g, n)
       end
     end
 
@@ -192,6 +52,207 @@ module Lemmas
       # Forcefully replace gender and number of the tag based on the original word gender and number
       # TODO: Investigate if it is better to perform the search with the correct tags to begin with
       result.copy(replace_tag_gn(result.tag, g, n))
+    end
+
+    private
+
+
+    # amiguiño => amigo
+    # enruguiñas => enruga
+    # albondiguiñas => albóndiga
+    # estomaguiño => estómago
+    def apply_gu(query, base, g, n)
+      tilde_variants("#{base.delete_suffix('u')}#{g}#{n}").map { |v| query.copy(v, @tags) }
+    end
+
+    # solciño => sol (Scms)
+    # animalciños => animal (Scmp)
+    # descalciña => descalzo
+    def apply_lc(query, base, g, n)
+      search_base = base.delete_suffix('c')
+      common_sp_query = query.copy("#{search_base}z#{g}#{n}", @tags)
+      if n == 's' # When plural
+        [query.copy("#{search_base}es", @tags), common_sp_query]
+      else
+        [query.copy(search_base, @tags), common_sp_query]
+      end
+    end
+
+    # quiño/a/os/as
+    # plaquiñas => placas
+    # retaquiños => retacos
+    # musiquiña => música
+    # musiquiño => músico
+    # faisquiñas => faíscas
+    # periodiquiños => periódicos
+    # politiquiñas => políticas
+    # bosquiños => bosques
+    def apply_qu(query, base, g, n)
+      search_base = "#{base.delete_suffix('qu')}c#{g}#{n}"
+      [query.copy(search_base, @tags),
+       *tilde_variants(search_base).map { |v| query.copy(v, @tags) },
+       query.copy(search_base, @v_tags),
+       query.copy("#{base}e#{n}", @tags)]
+    end
+
+    def apply_onc(query, base, g, n)
+      if g == 'o'
+        # algodonciño => algodón
+        # cartonciños => cartóns
+        query.copy("#{base.delete_suffix('onc')}ón#{n}", @tags)
+      else
+        # cancionciña => canción
+        # chaponciñas => chaponas
+        search_base = base.delete_suffix('onc')
+        [query.copy("#{search_base}ón#{n}", @sfc_a0f_tags),
+         query.copy("#{search_base}ona#{n}", @sfc_a0f_tags)]
+      end
+    end
+
+    # garavanciño => garavanzo
+    # crianciñas => crianzas
+    # vranciño => vran
+    def apply_anc(query, base, g, n)
+      search_base = base.delete_suffix('anc')
+      [query.copy("#{search_base}anz#{g}#{n}", @tags),
+       query.copy("#{search_base}án#{n}", @tags),
+       query.copy("#{search_base}an#{n}", @tags)]
+    end
+
+    # trenciñas => tranzas
+    # fervenciña => fervenza
+    def apply_enc_a(query, base, g, n)
+      query.copy("#{base.delete_suffix('c')}z#{g}#{n}", @tags)
+    end
+
+    # pinciñas => pinzas
+    # xardinciños => xardíns
+    def apply_inc(query, base, g, n)
+      search_base = base.delete_suffix('inc')
+      [query.copy("#{search_base}inz#{g}#{n}", @tags),
+       query.copy("#{search_base}ín#{n}", @tags)]
+    end
+
+    # segundiño => segundo
+    # brandiños => brandos
+    # pasandiño => pasando
+    # meirandiño => meirande
+    def apply_nd(query, base, g, n)
+      [query.copy("#{base}#{g}#{n}", @tags),
+       query.copy("#{base}e#{n}", @saiv_tags)]
+    end
+
+    def apply_rc(query, base, g, n)
+      # cogorciña => cogorza
+      # verciñas => verzas
+      # almorciño => almorzo
+      search_base = base.delete_suffix('c')
+      common_sp_query = query.copy("#{search_base}z#{g}#{n}", @sa_tags)
+      if n == 's' # When plural
+        # calamarciños => calamares
+        [common_sp_query, query.copy("#{search_base}es", @sa_tags)]
+      else
+        # calorciño => calor
+        # altarciño => altar
+        # amorciño => amor
+        [common_sp_query, query.copy(search_base, @sa_tags)]
+      end
+    end
+
+    # xefiño => xefe
+    # lumiño => lume
+    # suaviñas => suaves
+    # traxiños => traxes
+    def apply_fmvx(query, base, g, n)
+      [query.copy("#{base}#{g}#{n}", @sai_tags),
+       query.copy("#{base}e", @sai_tags)]
+    end
+
+    # gatiños => gatos
+    # azoutiña => azouta
+    # tomatiños => tomates
+    # amantiñas => amantes
+    def apply_t(query, base, g, n)
+      [query.copy("#{base}#{g}#{n}", @saiw_tags),
+       query.copy("#{base}e#{n}", @sav_tags)]
+    end
+
+    def apply_bpnh(query, base, g, n)
+      common_suffix_query = query.copy("#{base}#{g}#{n}", @sav_tags)
+      if base.end_with?('b')
+        # barbiña => barba
+        # xoubiñas => xouba
+        # FIXME: Intended rule was: nubiñas => nubes
+        # nubiñas => nube
+        [common_suffix_query, query.copy("#{base}e", @sav_tags)]
+      else
+        # trapiño => trapo
+        # castañiñas => castañas
+        # viñiño => viño
+        common_suffix_query
+      end
+    end
+
+    # estuchiño => estuche
+    # churriñas => churras
+    def apply_ch(query, base, g, n)
+      [query.copy("#{base}e", @sav_tags),
+       query.copy("#{base}#{g}#{n}", @sav_tags)]
+    end
+
+    # ovelliñas => ovellas
+    def apply_ll(query, base, g, n)
+      query.copy("#{base}#{g}#{n}", @sav_tags)
+    end
+
+    # aforriños => aforros
+    def apply_rr(query, base, g, n)
+      query.copy("#{base}#{g}#{n}", @sav_tags)
+    end
+
+    # cadradiños => cadrados
+    # todiñas => todas
+    # bigodiño => bigode
+    # humildiño => humilde
+    def apply_d(query, base, g, n)
+      [query.copy("#{base}#{g}#{n}", @saiv_tags),
+       query.copy("#{base}e#{n}", @saiv_tags)]
+    end
+
+    def apply_ues(query, base, g, n)
+      search_base = base.delete_suffix('es')
+      if n == ''
+        if g == 'o'
+          # marquesiño => marqués
+          query.copy("#{search_base}és", @sav_tags)
+        elsif g == 'a'
+          # marquesiña => marquesa
+          query.copy("#{search_base}esa", @sav_tags)
+        end
+      else # When plural
+        if g == 'o'
+          # marquesiños => marqueses
+          query.copy("#{search_base}eses", @sav_tags)
+        else
+          # marquesiñas => marquesas
+          query.copy("#{search_base}esas", @sav_tags)
+        end
+      end
+    end
+
+    # tesiños => tesos
+    # raposiña => raposa
+    # camisiñas => camisas
+    # tesiña => tese
+    # tosiña => tose
+    def apply_s(query, base, g, n)
+      [query.copy("#{base}#{g}#{n}", @sav_tags),
+       query.copy("#{base}e#{n}", @sav_tags)]
+    end
+
+    # horiña, peliños, groliño, etc.
+    def apply_default(query, base, g, n)
+      query.copy("#{base}#{g}#{n}", @tags)
     end
   end
 end
--- running/galician_xiada/lemmas/isimo_rule.rb
+++ running/galician_xiada/lemmas/isimo_rule.rb
@@ -21,58 +21,20 @@ module Lemmas
 
       base, g, n = Regexp.last_match.captures
 
-      if base.end_with?('bil')
-        # amabilísimo => amable
-        query.copy("#{base.delete_suffix('il')}le#{n}", @a_gn_tags[g][n])
-      elsif base.end_with?('qu')
-        # riquísimo => rico
-        query.copy("#{base.delete_suffix('qu')}c#{g}#{n}", @a_tags)
-      elsif base.end_with?('gu')
-        # vaguísimo => vago
-        query.copy("#{base.delete_suffix('u')}#{g}#{n}", @a_tags)
-      elsif base.end_with?('gü')
-        # ambigüísimo => ambiguo / pingüísimo => pingüe
-        search_base = base.delete_suffix('ü')
-        [query.copy("#{search_base}u#{g}#{n}", @a_tags),
-         query.copy("#{search_base}üe#{n}", @a_gn_tags[g][n])]
-      elsif base.end_with?('i')
-        # friísimo => frío
-        query.copy("#{base.delete_suffix('i')}í#{g}#{n}", @a_tags)
-      elsif base == 'cool'
-        # Plural exception: cool
-        query.copy(base, @a_gn_tags[g][n])
-      elsif base.end_with?('l')
-        # virtualísimo => virtual / facilísimo => fácil
-        if n == 's' # For plural
-          tilde_variants("#{base}es").map { |v| query.copy(v, @a_gn_tags[g][n]) }
-        else
-          tilde_variants(base).map { |v| query.copy(v, @a_gn_tags[g][n]) }
-        end
-      elsif base.end_with?('c')
-        # ferocísimo => feroz / docísimo => doce
-        search_base = base.delete_suffix('c')
-        sp_common_query = query.copy("#{search_base}z", @a_gn_tags[g][n])
-        if n == 's' # For plural
-          [sp_common_query, query.copy("#{search_base}ces", @a_gn_tags[g][n])]
-        else
-          [sp_common_query, query.copy("#{search_base}ce", @a_gn_tags[g][n])]
-        end
-      elsif base.end_with?('ad')
-        # preparadísimo => preparado
-        query.copy("#{base}#{g}#{n}", @av_tags)
-      elsif base.end_with?('id')
-        # convencidísimos => convencidos
-        # extendidísima => extendida
-        query.copy("#{base}#{g}#{n}", @av_tags)
-      elsif base.end_with?('t') && VALID_TISIMO.include?(base)
-        query.copy("#{base}#{g}#{n}", @av_tags)
-      elsif base.end_with?('s') && VALID_SISIMO.include?(base)
-        query.copy("#{base}#{g}#{n}", @av_tags)
-      else
-        # ísimo (default rule)
-        # listísimo => listo
-        # gravísimo => grave
-        query.copy("#{base}#{g}#{n}", @a_gn_tags[g][n])
+      case
+      when base.end_with?('bil') then apply_bil(query, base, g, n)
+      when base.end_with?('qu') then apply_qu(query, base, g, n)
+      when base.end_with?('gu') then apply_gu(query, base, g, n)
+      when base.end_with?('gü') then apply_gu_diaeresis(query, base, g, n)
+      when base.end_with?('i') then apply_i(query, base, g, n)
+      when base == 'cool' then apply_cool(query, base, g, n)
+      when base.end_with?('l') then apply_l(query, base, g, n)
+      when base.end_with?('c') then apply_c(query, base, g, n)
+      when base.end_with?('ad') then apply_ad(query, base, g, n)
+      when base.end_with?('id') then apply_id(query, base, g, n)
+      when base.end_with?('t') && VALID_TISIMO.include?(base) then apply_tisimo(query, base, g, n)
+      when base.end_with?('s') && VALID_SISIMO.include?(base) then apply_sisimo(query, base, g, n)
+      else apply_default(query, base, g, n)
       end
     end
 
@@ -85,6 +47,89 @@ module Lemmas
       tag = replace_tag_gn(result.tag.sub(/\AA0/, 'As'), g, n)
 
       result.copy(replace_tag_gn(tag, g, n))
+    end
+
+    private
+
+    # amabilísimo => amable
+    def apply_bil(query, base, g, n)
+      query.copy("#{base.delete_suffix('il')}le#{n}", @a_gn_tags[g][n])
+    end
+
+    # riquísimo => rico
+    def apply_qu(query, base, g, n)
+      query.copy("#{base.delete_suffix('qu')}c#{g}#{n}", @a_tags)
+    end
+
+    # vaguísimo => vago
+    def apply_gu(query, base, g, n)
+      query.copy("#{base.delete_suffix('u')}#{g}#{n}", @a_tags)
+    end
+
+    # ambigüísimo => ambiguo
+    # pingüísimo => pingüe
+    def apply_gu_diaeresis(query, base, g, n)
+      search_base = base.delete_suffix('ü')
+      [query.copy("#{search_base}u#{g}#{n}", @a_tags),
+       query.copy("#{search_base}üe#{n}", @a_gn_tags[g][n])]
+    end
+
+    # friísimo => frío
+    def apply_i(query, base, g, n)
+      query.copy("#{base.delete_suffix('i')}í#{g}#{n}", @a_tags)
+    end
+
+    # Plural exception: cool
+    def apply_cool(query, base, g, n)
+      query.copy(base, @a_gn_tags[g][n])
+    end
+
+    # virtualísimo => virtual
+    # facilísimo => fácil
+    def apply_l(query, base, g, n)
+      if n == 's' # For plural
+        tilde_variants("#{base}es").map { |v| query.copy(v, @a_gn_tags[g][n]) }
+      else
+        tilde_variants(base).map { |v| query.copy(v, @a_gn_tags[g][n]) }
+      end
+    end
+
+    # ferocísimo => feroz
+    # docísimo => doce
+    def apply_c(query, base, g, n)
+      search_base = base.delete_suffix('c')
+      sp_common_query = query.copy("#{search_base}z", @a_gn_tags[g][n])
+      if n == 's' # For plural
+        [sp_common_query, query.copy("#{search_base}ces", @a_gn_tags[g][n])]
+      else
+        [sp_common_query, query.copy("#{search_base}ce", @a_gn_tags[g][n])]
+      end
+    end
+
+    # preparadísimo => preparado
+    def apply_ad(query, base, g, n)
+      query.copy("#{base}#{g}#{n}", @av_tags)
+    end
+
+    # convencidísimos => convencidos
+    # extendidísima => extendida
+    def apply_id(query, base, g, n)
+      query.copy("#{base}#{g}#{n}", @av_tags)
+    end
+
+    def apply_tisimo(query, base, g, n)
+      query.copy("#{base}#{g}#{n}", @av_tags)
+    end
+
+    def apply_sisimo(query, base, g, n)
+      query.copy("#{base}#{g}#{n}", @av_tags)
+    end
+
+    # ísimo (default rule)
+    # listísimo => listo
+    # gravísimo => grave
+    def apply_default(query, base, g, n)
+      query.copy("#{base}#{g}#{n}", @a_gn_tags[g][n])
     end
   end
 end
