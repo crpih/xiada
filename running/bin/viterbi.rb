crpih/xiada

commit d68e3c19ef5a9e54dbdd567dee9928c66b7ab788

Excluding hiperlemmas from proper nouns.

running/bin/viterbi.rb
@@ -874,7 +874,13 @@ class Viterbi
             puts "<#{hiperlemma_tag}>#{tag_object.hiperlemmas[lemma]}</#{hiperlemma_tag}>" if hiperlemma_tag
           else
             puts "<#{lemma_tag}>#{StringUtils.replace_xml_conflicting_characters(lemma)}</#{lemma_tag}>"
-            puts "<#{hiperlemma_tag}>#{StringUtils.replace_xml_conflicting_characters(tag_object.hiperlemmas[lemma])}</#{hiperlemma_tag}>" if hiperlemma_tag
+            if hiperlemma_tag
+              if tag_object.hiperlemmas[lemma]
+                puts "<#{hiperlemma_tag}>#{StringUtils.replace_xml_conflicting_characters(tag_object.hiperlemmas[lemma])}</#{hiperlemma_tag}>"
+              else
+                puts "<#{hiperlemma_tag}></#{hiperlemma_tag}>"
+              end
+            end
           end
           puts "</#{tag_lemma_tag}>"
         end

running/bin/database_wrapper.rb
@@ -121,7 +121,6 @@ class DatabaseWrapper
     end
     return result
   end
-
   def get_bigram_probability(tag_j, tag_k)
     result = @db.get_first_value("select log_ajk from bigram_frequencies where tj='#{SQLUtils.escape_SQL(tag_j)}' and tk='#{SQLUtils.escape_SQL(tag_k)}'")
     if result == nil
@@ -141,11 +140,11 @@ class DatabaseWrapper
         #STDERR.puts "Unigram found:#{tag_k} probability:#{result}"
         return Float(result)
       else
-        # STDERR.puts "Bigram found:#{tag_j},#{tag_k} probability:#{result}"
+        #STDERR.puts "Bigram found:#{tag_j},#{tag_k} probability:#{result}"
         return Float(result)
       end
     else
-      # STDERR.puts "Trigram found:#{tag_i},#{tag_j},#{tag_k} probability:#{result}"
+      #STDERR.puts "Trigram found:#{tag_i},#{tag_j},#{tag_k} probability:#{result}"
       return Float(result)
     end
   end

running/bin/propernouns_processor.rb
@@ -108,7 +108,7 @@ class ProperNounsProcessor
         #STDERR.puts "standard token: #{token.text} type:#{token.token_type}"
         if (StringUtils.first_only_upper?(token.text) or StringUtils.alone_letter_upper?(token.text) or
             StringUtils.propers_joined?(token.text)) and !token.tagged?
-          token.add_tags_lemma_emission(@candidate_tags, token.text, token.text, 0.0, false)
+          token.add_tags_lemma_emission(@candidate_tags, token.text, nil, 0.0, false)
           #STDERR.puts "inside"
           # Beginning of a standard proper noun
         end
@@ -248,7 +248,7 @@ class ProperNounsProcessor
     #STDERR.puts "joining standard proper noun from:#{from.text} to #{to.text}"
     token = join_proper_noun(from, to)
     #STDERR.puts "token.text: #{token.text}"
-    token.add_tags_lemma_emission(@candidate_tags, token.text, token.text, 0.0, false)
+    token.add_tags_lemma_emission(@candidate_tags, token.text, nil, 0.0, false)
     # if the token is in uppercase in the lexicon, we add the other tags too.
     results = @dw.get_tags_lemmas_emissions_strict(token.text, nil)
     #STDERR.puts "results:#{results}"
@@ -496,7 +496,7 @@ class ProperNounsProcessor
 
   def process_regexp_proper_nouns_recursive(token, way, ways)
     if token.token_type == :standard
-      token.add_tags_lemma_emission(@candidate_tags, token.text, token.text, 0.0, false) if regexp_proper_noun(token) and !token.tagged?
+      token.add_tags_lemma_emission(@candidate_tags, token.text, nil, 0.0, false) if regexp_proper_noun(token) and !token.tagged?
       process_regexp_proper_nouns_recursive(token.next, way, ways)
     elsif token.token_type == :begin_alternative
       # Follow all ways recursively

commit e80df3dfae06958ed82f1ff51686f775bd4739fe

Excluding hiperllemas from proper nouns (fix bug introduced in d68e3c19ef5a9e54dbdd567dee9928c66b7ab788).

running/bin/viterbi.rb
@@ -871,7 +871,13 @@ class Viterbi
           puts "<#{tag_tag}>#{tag}</#{tag_tag}>"
           if token.chunk_entity_exclude_transform
             puts "<#{lemma_tag}>#{lemma}</#{lemma_tag}>"
-            puts "<#{hiperlemma_tag}>#{tag_object.hiperlemmas[lemma]}</#{hiperlemma_tag}>" if hiperlemma_tag
+            if hiperlemma_tag
+              if tag_object.hiperlemmas[lemma]
+                puts "<#{hiperlemma_tag}>#{tag_object.hiperlemmas[lemma]}</#{hiperlemma_tag}>"
+              else
+                puts "<#{hiperlemma_tag}></#{hiperlemma_tag}>"
+              end
+            end
           else
             puts "<#{lemma_tag}>#{StringUtils.replace_xml_conflicting_characters(lemma)}</#{lemma_tag}>"
             if hiperlemma_tag
@@ -911,10 +917,22 @@ class Viterbi
           tag_object.lemmas.keys.each do |lemma|
             if token.chunk_entity_exclude_transform
               puts "<#{lemma_tag}>#{lemma}</#{lemma_tag}>"
-              puts "<#{hiperlemma_tag}>#{tag_object.hiperlemmas[lemma]}</#{hiperlemma_tag}>" if hiperlemma_tag
+              if hiperlemma_tag
+                if tag_object.hiperlemmas[lemma]
+                  puts "<#{hiperlemma_tag}>#{tag_object.hiperlemmas[lemma]}</#{hiperlemma_tag}>"
+                else
+                  puts "<#{hiperlemma_tag}></#{hiperlemma_tag}>"
+                end
+              end
             else
               puts "<#{lemma_tag}>#{StringUtils.replace_xml_conflicting_characters(lemma)}</#{lemma_tag}>"
-              puts "<#{hiperlemma_tag}>#{StringUtils.replace_xml_conflicting_characters(tag_object.hiperlemmas[lemma])}</#{hiperlemma_tag}>" if hiperlemma_tag
+              if hiperlemma_tag
+                if tag_object.hiperlemmas[lemma]
+                  puts "<#{hiperlemma_tag}>#{StringUtils.replace_xml_conflicting_characters(tag_object.hiperlemmas[lemma])}</#{hiperlemma_tag}>"
+                else
+                  puts "<#{hiperlemma_tag}></#{hiperlemma_tag}>"
+                end
+              end
             end
           end
         end