5e7dfe86fb2091cd10fd2423eab57d0ee3a1c83a
david <david@nlpgo.com>
2024-09-03 09:25:27 +0200

    Simplify construction process.

--- build.sh
+++ build.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-cd training/bin
-make galician_xiada_escrita galician_xiada_oral spanish_eslora
-cd ../..
-
 DOCKER_BUILDKIT=1 docker build --ssh default -t xiada_tagger:latest .
--- running/bin/client.rb
+++
@@ -1,6 +0,0 @@
-require_relative 'tag_text'
-
-text = ARGV.join(" ")
-tag_text(text, nil).each do |t|
-  puts "#{t[:token]}\t#{t[:tag]}\t#{t[:lemma]}\t#{t[:hiperlemma]}\t#{text[t[:start]..t[:finish]]}"
-end
