33d8d8bf1d9a03ed0fc196fa142532b29e088341
david <david@nlpgo.com>
2025-05-20 14:09:09 +0200

    Refactor: move call method to private scope in xiada_tagger.rb

--- running/bin/xiada_tagger.rb
+++ running/bin/xiada_tagger.rb
@@ -32,9 +32,9 @@ class XiadaTagger
     texts.map { |t| t.empty? ? [] : tag_text(document_config, trained_proper_nouns, t).all_ways }
   end
 
-  def call(document_config, text) = tag_text(document_config, nil, text)
-
   private
+
+  def call(document_config, text) = tag_text(document_config, nil, text)
 
   def tag_text(document_config, proper_nouns_processor, text)
     sentence = Sentence.new(
